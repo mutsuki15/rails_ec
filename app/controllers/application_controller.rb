8f5ace659b77a57c36cbbd507afb637ce89898ce
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2023-11-10 21:51:04 +0900

    指摘事項の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   def current_cart
-    current_cart = Customer.find_by(id: session[:customer_id])
-    current_cart ||= Customer.create
-    session[:customer_id] = current_cart.id
+    current_cart = Customer.find_or_create_by(id: session[:customer_id])
+    session[:customer_id] ||= current_cart.id
     current_cart
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :set_cart
+
   def index
     @products = Product.all
-    @cart_item = CartItem.new
-    @cart_items = current_cart.cart_items.includes([:product])
   end
 
   def show
     @product = Product.find(params[:id])
     @related_products = Product.all.where.not(id: @product).order(updated_at: :asc).first(4)
+  end
+
+  private
+
+  def set_cart
     @cart_item = CartItem.new
     @cart_items = current_cart.cart_items.includes([:product])
   end
