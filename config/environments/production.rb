0530a1c0048a2cc3df9b9a43e2f9f90ec4af2183
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2023-11-24 14:29:58 +0900

    アプリ名の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'rails-ec-mutsuki.herokuapp.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'rails-ec-mutsuki-f2bbf999e898.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
