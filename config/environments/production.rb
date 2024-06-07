31e731f36405fb037f5c040525dd92a02f1e837b
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-06-08 02:45:41 +0900

    キーの更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,8 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+
+  config.github_client_id = ENV['GITHUB_CLIENT_ID']
+  config.github_client_secret = ENV['GITHUB_CLIENT_SECRET']
+
 end
